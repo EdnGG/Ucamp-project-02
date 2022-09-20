31b2c5cf88f95d84b164a1c1c776c3c4d1cd2f86
Eden <gresseden@gmail.com>
2022-09-20 12:56:55 -0400

    cleaning some code

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ input.setAttribute("id", "input");
 d.addEventListener("DOMContentLoaded", () => {
     console.log("DOM loaded");
     createInput();
-    createTitleTable();
+    createTitle();
     addTask();
   if (!localStorage.getItem("tasks")) {
     console.log("No tasks");
@@ -50,9 +50,29 @@ const addTask = () => {
     console.log("arrayOfTasks: ", arrayOfTasks);
     input.value = "";
 
+    
+
     saveToLocalStorage();
     showTasks();
   });
+};
+
+const showTasks = () => {
+  arrayOfTasks.forEach((task) => {
+      divTasks.innerHTML += `
+        <div class="task">
+          <div class="task__title">
+            <h2>${task}</h2>
+          </div>
+          <div class="task__buttons">
+            <button class="task__buttons--edit">Edit</button>
+            <button class="task__buttons--delete">Delete</button>
+          </div>
+        </div>
+      `;
+      secondaryDiv.appendChild(divTasks);
+  });  
+  
 };
 
 const saveToLocalStorage = () => {
@@ -71,66 +91,9 @@ const updateFromLocalStorage = (task) => {
   saveToLocalStorage();
 };
 
-const showTasks = () => {
-
-  
-  arrayOfTasks.forEach((task) => {
-
-    // tasksList.innerHTML += `
-    //   <div class="task">
-    //     <div class="task__title">
-    //       <h2>${task}</h2>
-    //     </div>
-    //     <div class="task__buttons">
-    //       <button class="task__buttons--edit">Edit</button>
-    //       <button class="task__buttons--delete">Delete</button>
-    //     </div>
-    //   </div>
-    // `;
-
-    // let secondDiv = d.getElementById("secondaryDiv")
-    // secondaryDiv.appendChild(tasksList);
-    // divTasks.innerHTML = tasksList;
-
-    // divTasks.appendChild(tasksList);
-    // secondaryDiv.appendChild(divTasks);
 
 
-      
-      // let divContent = d.createElement("div");
-      // let buttonEdit = d.createElement("button");
-      // let buttonDelete = d.createElement("button");
-  
-      // buttonEdit.setAttribute("id", "buttonEdit");
-      // buttonDelete.setAttribute("id", "buttonDelete");
-      // divContent.setAttribute("id", "divContent");
-  
-      // divTasks.innerHTML = task;
-      divTasks.innerHTML += `
-        <div class="task">
-          <div class="task__title">
-            <h2>${task}</h2>
-          </div>
-          <div class="task__buttons">
-            <button class="task__buttons--edit">Edit</button>
-            <button class="task__buttons--delete">Delete</button>
-          </div>
-        </div>
-      `;
-      // buttonEdit.innerHTML = "Edit";
-      // buttonDelete.innerHTML = "Delete";
-  
-      // divTasks.appendChild(buttonEdit);
-      // divTasks.appendChild(buttonDelete);
-  
-      // divTasks.appendChild(tasksList);
-      secondaryDiv.appendChild(divTasks);
-      // d.getElementById("secondaryDiv").appendChild(divContent);
-  });  
-  
-};
-
-const createTitleTable = () => {
+const createTitle = () => {
   let divTitle = d.createElement("h1");
   let secondaryDiv = d.getElementById("secondaryDiv");
   divTitle.innerHTML = "CRUD";
